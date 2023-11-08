e1f51fe43f9acd0f4b4cdad0945ceac8a01dddef
Kaylee Chan <chankay5@teach.cs.toronto.edu>
2023-11-07 23:33:42 -0500

    Initial P2 commit

---
+++ P2_Root/startup.sh
@@ -0,0 +1,16 @@
+python3 -m venv venv
+source venv/bin/activate
+
+pip install Django
+pip install djangorestframework
+pip install markdown
+pip install django-filter
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+
+pip install djangorestframework-simplejwt
+
+chmod +x manage.py
+./manage.py makemigrations
+./manage.py migrate
