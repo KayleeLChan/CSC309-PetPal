f7ba630ce86c6cb4e1d268279cab23667da9b6c0
minha-faheem <minha.faheem@mail.utoronto.ca>
2023-11-13 22:00:09 -0500

    IDK why i see non-committed things here

--- P2_Root/applications/urls.py
+++ P2_Root/applications/urls.py
@@ -4,8 +4,8 @@ from .views import
 app_name='applications'
 
 urlpatterns = [
-    path('<int:pk>/new/', CreateApplicationView.as_view(), name='application-create'),
-    path('updated/<int:pk>/', UpdateApplicationView.as_view(), name='application-update'),
+    path('new/<int:pk>/', CreateApplicationView.as_view(), name='application-create'),
+    path('editor/<int:pk>/', UpdateApplicationView.as_view(), name='application-update'),
     path('list/<int:pk>/', ListApplicationView.as_view(), name='application-list'),
     path('details/<int:pk>/', GetApplicationView.as_view(), name='application-get')
 ]
