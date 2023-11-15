f24c4c0e14d9179858e26d121ffae07f142934e3
George Meghan Quan <georg267@teach.cs.toronto.edu>
2023-11-15 15:14:19 -0500

    sstartup

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,5 +1,6 @@
 python3 -m venv venv
 virtualenv venv
+# source venv bin activate does not work on startup.sh, if venv does not activate properly, please do this manually
 source venv/bin/activate
 
 python3 -m pip install --upgrade pip
@@ -8,9 +9,6 @@ pip install Django
 pip install djangorestframework
 pip install markdown
 pip install django-filter
-
-
-
 pip install djangorestframework-simplejwt
 pip install django-multiselectfield
 
