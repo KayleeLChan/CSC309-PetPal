a63263402a2ab5628c2e4e5432f4e41799c85c8d
George Meghan Quan <georg267@teach.cs.toronto.edu>
2023-11-15 15:12:21 -0500

    eddit startup

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,14 +1,15 @@
-# python3 -m venv venv
+python3 -m venv venv
 virtualenv venv
 source venv/bin/activate
 
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
 pip install Django
 pip install djangorestframework
 pip install markdown
 pip install django-filter
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade Pillow
+
 
 pip install djangorestframework-simplejwt
 pip install django-multiselectfield
