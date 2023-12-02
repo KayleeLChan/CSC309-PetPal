b9420de529c57681b2a16f2f5846715bd3edab1d
mqg88 <meghan.george@mail.utoronto.ca>
2023-12-02 14:52:23 -0500

    LOGIN WORKS i think

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/Home/Index";
 import Notification from "./pages/Notification/Index";
+import Login from "./pages/Login/Index";
 import Error404 from "./pages/404/Index";
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="notifications" element={<Notification />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
--- frontend/src/components/loginform.jsx
+++ frontend/src/components/loginform.jsx
@@ -1,1 +1,38 @@
 import React from 'react';
+
+function LoginForm(props) {
+
+        function handleSubmit(event) {
+          event.preventDefault();
+          props.handle_login(event);
+        }
+
+  return (
+      <form className="w-100 align-items-center" onSubmit={handleSubmit}>
+        <h1 className="text-primary-cream">Welcome Back!</h1>
+        <div className="form-group row text-primary-cream">
+          <label className="row-form-label h5" htmlFor="username">Username</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control bg-primary-cream font-plain" id="username" name="username" placeholder="username123" required />
+          </div>
+        </div> 
+        <div className="form-group row text-primary-cream">
+          <label className="row-form-label h5" htmlFor="password">Password</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control bg-primary-cream font-plain" id="password" name="password" required />
+          </div>
+        </div>
+        <p class="smallpar">{props.error}</p>
+        <button type="submit" className="btn btn-lg btn-primary-orange m-3 shadow-sm" required>Login</button>
+            <div className="signupcontainer">
+                  <p className="h6 font-plain text-primary-cream">Don't have an account?</p>
+                  <button type="button" className="btn btn-lg btn-primary-cream m-3 shadow-sm">Sign Up</button>
+            </div>
+      </form> 
+  );
+}
+
+export default LoginForm;
+
+
+
