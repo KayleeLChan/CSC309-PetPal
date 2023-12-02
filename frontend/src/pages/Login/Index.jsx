f220509ef51eb087c5863fb538f43f439bce4ce1
mqg88 <meghan.george@mail.utoronto.ca>
2023-12-02 00:05:24 -0500

    commit

---
+++ frontend/src/pages/Login/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import React from 'react';
+
+function Login() {
+    return (
+        <>
+            <div data-bs-theme="petpal">
+                <div className="main">
+                    [CONTENT]
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login;
